c22a8dc2978f0b86f12741f1c4e0e6f6b97291ef
smol <jiroshimonis@gmail.com>
2022-08-07 16:04:46 +0800

    now go

---
+++ ins.sh
@@ -0,0 +1,20 @@
+# yes
+cp ./everything/* /system/bin
+
+# hmm unyes
+echo "
+done
+
+you really expect it to be harder when it literally just copies the 8 aliases to /system/bin
+
+oh and if it returns an RO only file-system error then remount /system as RW in termux
+
+if you cant do that root your phone
+
+anyways so long"
+
+# back to home dir
+cd ~
+
+# goodbye friend
+rm -rf als
